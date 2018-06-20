3ebe2b02eeac9c3851eb0cb8de5b73b0be436151
Tim <timothyhsieh27@gmail.com>
2018-06-19 21:29:47 -0400

    Realigned class Square

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  
-  render() {
+  class Square extends React.Component {
+    render() {
       return (
         <button className="square" onClick={() => this.props.onClick()}>
           {this.props.value}
